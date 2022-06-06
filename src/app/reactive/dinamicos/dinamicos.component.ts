b0876bb29807f12f901c371dee1f67229b59711f
lzamranoa <lzambranoa.software@gmail.com>
2022-06-06 18:56:41 -0500

    Creación del FormArray,  creacion de los controles del FormArray, implementación de la funcion agregar y borrar un juego favorito

--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormArray, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -11,13 +11,39 @@ export class DinamicosComponent {
 
   miFormulario: FormGroup = this.fb.group({
     nombre: ['', [Validators.required, Validators.minLength(3)]],
-  })
+    favoritos: this.fb.array([
+      ['Metal Hero', Validators.required],
+      ['Super mario Bros III', Validators.required]
+    ], Validators.required)
+  });
+
+  nuevoFavorito: FormControl = this.fb.control('', Validators.required)
+
+  get favoritosArr() {
+    return this.miFormulario.get('favoritos') as FormArray
+  }
 
   constructor(private fb: FormBuilder) { }
 
   campoNoValido(campo: string) {
     return this.miFormulario.controls[campo].errors 
         && this.miFormulario.controls[campo].touched
+  }
+
+  agregarFavorito(){
+    if(this.nuevoFavorito.invalid){ return; }
+
+    // this.favoritosArr.push( new FormControl( this.nuevoFavorito.value, Validators.required) );
+    // Misma fucnion con el FormBuilder
+    this.favoritosArr.push(this.fb.control( this.nuevoFavorito.value, Validators.required) );
+
+
+    // Este comando me pérmite dejar el formulario en balnco
+    this.nuevoFavorito.reset();
+  }
+
+  borrar(i: number) {
+    this.favoritosArr.removeAt(i)
   }
 
   guardar(){
