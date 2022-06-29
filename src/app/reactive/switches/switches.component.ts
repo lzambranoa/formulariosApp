6df1b2b434c7fe78587d76e286a04be8f301b8b0
lzamranoa <lzambranoa.software@gmail.com>
2022-06-28 20:07:07 -0500

    Alamcenamiento de los datos del formulario de switches reactivos

--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
@@ -8,9 +9,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwitchesComponent implements OnInit {
 
-  constructor() { }
+  miFormulario: FormGroup = this.fb.group({
+    genero: ['M', Validators.required],
+    notificaciones: [true, Validators.required],
+    condiciones: [false, Validators.requiredTrue]  // Valida que sea obligatoriamente true
+  });
+
+  persona = {
+    genero :'F',
+    notificaciones : true 
+  }
+
+
+  constructor( private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.miFormulario.reset({
+      ...this.persona,
+      condiciones: false
+    });
+
+    //  metodo para la suscribcion de los cambios dados en el formulario
+    // extraigo las condiciones como una variable independiente
+    this.miFormulario.valueChanges.subscribe( ({condiciones, ...rest}) => {
+      // delete form.condiciones;
+      this.persona = rest;
+    })
   }
+
+  guardar() {
+    const formValue = {...this.miFormulario.value};
+    delete formValue.condiciones;
+
+    this.persona = formValue;
+  }
+
+ 
 
 }
